d76fe168319b1698400797a054d576c98165c05f
Emma Mora <emmanu20021@gmail.com>
2023-09-28 01:19:01 -0600

    Primer avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  imgProfile = 'https://lh3.googleusercontent.com/a-/AOh14Gj4g_876K0u0zpeklckQhSIZZR_xOHufFhujcQxdA=s96-c';
   title = 'listicle';
 }
